a8edcfb724e5790d0cb70b1af8151b109efb4876
Bregy Malpartida Ramos <bregy.malpartida@utec.edu.pe>
2018-10-08 07:08:31 -0500

    added more images and more slides

--- components/BregyLogo.js
+++ components/BregyLogo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const bregyLogo = '../images/bregyLogo.png';
+const bregyLogo =
+	'https://github.com/bregydoc/whoiam-pitch/raw/master/images/bregymalpartida.png';
 
 const LogoWrapper = styled.img`
 	width: auto;
-	height: 200px;
+	height: 50vh;
 `;
 const BregyLogo = props => {
 	return <LogoWrapper {...props} src={bregyLogo} />;
